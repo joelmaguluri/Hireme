199080d81bbaa05ef6ca28faf98b5cf5f8fc2339
sudeep745 <msudeep.joel@gmail.com>
2019-02-19 19:49:09 +0530

    Mailer

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,6 +1,7 @@
 var app=require('express');
 var router = app.Router();
 var bodyParser=require('body-parser');
+const email="msudeep.joel@gmail.com";
 const nodemailer = require("nodemailer");
 router.get('/login',function(req,res,next){
 res.send("helloworld");
@@ -18,34 +19,39 @@ router.post('/connectwithme',function(req,res,next){
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "gains.arrowsupercloud.com",
+    host: "smtp.gmail.com",
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: 'thisissudeep@msudeepjoel.com', // generated ethereal user
-      pass: 'AMMAHello123@#' // generated ethereal password
+      user: 'msudeep.joel@gmail.com', // generated ethereal user
+      pass: 'AmmaNana789)!' // generated ethereal password
     }
   });
   // setup email data with unicode symbols
+ let name=req.body.fname+req.body.lname;
  let mailOptions = {
-   from: '"Sudeep Joel" <thisissudeep@msudeepjoel.com>', // sender address
-   to:`msudeepjoel.joel@gmail.com`, // list of receivers
-   subject: `Name:${req.body.fname}${req.body.fname}`, // Subject line
+   from: '"Sudeep Joel" <msudeep.joel@gmail.com>', // sender address
+   to:`${email}`,// list of receivers
+   subject: `Name:${name}`, // Subject line
    text: `${req.body.message}`, // plain text body
-   html: `<b>Your Details<b><br><b>Firstname:${req.body.fname}</b><br>
+   html: `<b>Your Details<b><br><b>Firstname:${name}</b><br>
    <b>Lastename:${req.body.lname}</b><br><b>Email:${req.body.email}</b><br>
    Message:${req.body.message}</b><br>
    Subject:${req.body.subject}</b><br>` // html body
  };
 let info = transporter.sendMail(mailOptions,function(error, response){
-    if(error){
-        console.log(error);
-    }else{
-      console.log("Message sent: %s", info.messageId);
-// Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    }
-});
+      if(error){
+          console.log('Failed in sending mail');
+          console.dir({success: false, existing: false, sendError: true});
+          console.dir(error);
+          res.end('Failed in sending mail');
+      }else{
+          console.log('Successful in sending email');
+          console.dir({success: true, existing: false, sendError: false});
+          console.dir(response);
+          res.end('Successful in sending email');
+      }
+  });
     res.send(req.body);
 })
 module.exports = router;
