895cc730ec5e710e834a0f8d12483cbc9ac3bab1
sudeep745 <msudeep.joel@gmail.com>
2019-02-19 14:34:45 +0530

    process-env-port

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ app.use(express.static('views'));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/users',userRouter);
 app.use('/admin',adminRouter);
-var port = process.env.port || 3000;
-app.listen(port,function(err){
-    if(err){
-        throw err;
-    }
-    console.log("Server is Running at  port 3000!")
+var port=process.env.PORT||3000;
+app.listen(port, function(err){
+    if(err) throw err;
+    console.log("Server is running");
 });
+
 /* GET home page. */
