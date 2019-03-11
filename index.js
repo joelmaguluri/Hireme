33f1d3b915030a783796782007ad29b683a9607a
sudeep987 <msudeep.joel@gmail.com>
2019-03-11 22:34:44 +0530

    Hireme

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ var userRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var bodyParser=require('body-parser');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('views'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads'));
 app.use('/users',userRouter);
 app.use('/admin',adminRouter);
-var port=process.env.PORT||3000;
+var port=process.env.PORT||4000;
 app.listen(port, function(err){
     if(err) throw err;
-    console.log("Server is running 3000");
+    console.log("Server is running 4000");
 });
 
 /* GET home page. */
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,62 +1,73 @@
 var app=require('express');
 var router = app.Router();
 var bodyParser=require('body-parser');
-const email="msudeep.joel@gmail.com";
 const nodemailer = require("nodemailer");
-
-/*router.get('/login',function(req,res,next){
-res.send("helloworld");});
-
-router.get('/register',function(req,res,next){
-res.send("helloworld");
-
-})*/
-router.get('/sendemail',function(req,res,next){
- res.sendFile('/views/index.html', { root: "./"});
-});
-router.post('/sendemail',function(req,res,next){
-console.log(req.body);
-res.json(req.body);
-});
-/*router.post('/sendemail',function(req,res,next){
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: 'msudeep.joel@gmail.com', // generated ethereal user
-      pass: 'AmmaNana789)!' // generated ethereal password
+var multer = require('multer');
+const path=require('path');
+const fs=require('fs')
+var email='msudeep.joel@gmail.com'
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname+'.pdf',()=>{
+      alert('Not pdf');
+    })
+  },
+  fileFilter: function (req, file, cb) {
+    if (path.extension(file.originalname) !== '.pdf') {
+      return cb(null, false)
     }
-  });
-  // setup email data with unicode symbols
- let name=req.body.fname+req.body.lname;
- let mailOptions = {
-   from: '"Sudeep Joel" <msudeep.joel@gmail.com>', // sender address
-   to:`${email}`,// list of receivers
-   subject: `Resume:${name}`, // Subject line
-   text: ``, // plain text body
-   html: `<p>${req.body.message}</p>`, // html body
-   attachments: [
-   {
-    path: ABSPATH + '/images/test.jpg'
-   }]
- };
-let info = transporter.sendMail(mailOptions,function(error, response){
-      if(error){
-          console.log('Failed in sending mail');
-          console.dir({success: false, existing: false, sendError: true});
-          console.dir(error);
-          res.end('Failed in sending mail');
-      }else{
-          console.log('Successful in sending email');
-          console.dir({success: true, existing: false, sendError: false});
-          console.dir(response);
-          res.end('Successful in sending email');
-      }
-  });
-    res.send(req.body);
-});*/
 
+    cb(null, true)
+  }
+});
+
+var upload = multer({ storage: storage });
+router.get('/sendemail',function(req,res,next){
+ res.sendFile( path.join(__dirname, '/../../views/index.html') );
+});
+
+
+ router.post('/sendemail', upload.single('Resume'), function(req, res,next) {
+   let transporter = nodemailer.createTransport({
+     host: "smtp.gmail.com",
+     port: 465,
+     secure: true, // true for 465, false for other ports
+     auth: {
+       user: 'msudeep.joel@gmail.com', // generated ethereal user
+       pass: '*****' // generated ethereal password
+     }
+   });
+   // setup email data with unicode symbols
+  let name=req.body.fname+req.body.lname;
+  let mailOptions = {
+    from: '"Sudeep Joel" <msudeep.joel@gmail.com>', // sender address
+    to:`${email}`,// list of receivers
+    subject: `Resume:${req.name}`, // Subject line
+    text: `helloworld`, // plain text body
+    html: `<p>${req.body.message}</p>`, // html body
+    attachments: [
+         {   // utf-8 string as an attachment
+             filename: 'Resume.pdf',
+             content: fs.createReadStream('../../uploads/Resume.pdf')
+         }]
+  };
+ let info = transporter.sendMail(mailOptions,function(error, response){
+       if(error){
+           console.log('Failed in sending mail');
+           console.dir({success: false, existing: false, sendError: true});
+           console.dir(error);
+           res.end('Failed in sending mail');
+       }else{
+           console.log('Successful in sending email');
+           console.dir({success: true, existing: false, sendError: false});
+           console.dir(response);
+           res.end('Successful in sending email');
+       }
+   });
+  console.log(req.file);
+ res.send("file saved on server");
+ });
 module.exports = router;
